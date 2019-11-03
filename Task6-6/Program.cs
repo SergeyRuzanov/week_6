2a79c3a713d96595725dc4e555d8866e6fc7de56
Sergey Ruzanov <SRuzanov84@gmail.com>
2019-11-03 21:23:32 +0500

    1

--- Task6-6/Program.cs
+++ Task6-6/Program.cs
@@ -6,46 +6,64 @@ namespace Task6_3
 {
     class Program
     {
-        static bool IsSerchTree(string[][] list, int index)
+        enum Direction
         {
-            if (index == -1)
-                return true;
-            int count = int.Parse(list[index][0]);
-            int leftChildIndex = int.Parse(list[index][1]);
-            int rightChildIndex = int.Parse(list[index][2]);
+            right,
+            left
+        }
 
-            if (IsSerchTree(list, int.Parse(list[index][1]) - 1) && IsSerchTree(list, int.Parse(list[index][2]) - 1))
+        static bool CheckSubtree(int[,] list, int index0, Direction direction, Stack<int> stack)
+        {
+            int index;
+            if (direction == Direction.left)
+                index = list[index0, 1];
+            else
+                index = list[index0, 2];
+
+            if (index != -1)
+                stack.Push(index);
+
+            while (stack.Count != 0)
             {
-                if (leftChildIndex > 0 && rightChildIndex > 0)
+                index = stack.Pop();
+                if (direction == Direction.left)
                 {
-                    if (count > int.Parse(list[leftChildIndex - 1][0]) && count < int.Parse(list[rightChildIndex - 1][0]))
-                        return true;
-                    else
-                        return false;
-                }
-                else if (leftChildIndex != 0)
-                {
-                    if (count > int.Parse(list[leftChildIndex - 1][0]))
-                        return true;
-                    else
-                        return false;
-                }
-                else if (rightChildIndex != 0)
-                {
-                    if (count < int.Parse(list[rightChildIndex - 1][0]))
-                        return true;
-                    else
+                    if (list[index, 0] > list[index0, 0])
                         return false;
                 }
                 else
                 {
-                    return true;
+                    if (list[index, 0] < list[index0, 0])
+                        return false;
                 }
+                if (list[index, 2] != -1)
+                    stack.Push(list[index, 2]);
+                if (list[index, 1] != -1)
+                    stack.Push(list[index, 1]);
             }
-            else
+            return true;
+        }
+        static bool IsSerchTree(int[,] list)
+        {
+            if (list[0, 1] == -1 && list[0, 2] == -1)
             {
-                return false;
+                return true;
             }
+            int index0 = 0;
+            Stack<int> stack = new Stack<int>();
+            Stack<int> stackOut = new Stack<int>();
+            stackOut.Push(index0);
+            while (stackOut.Count != 0)
+            {
+                index0 = stackOut.Pop();
+                if (!CheckSubtree(list, index0, Direction.left, stack) || !CheckSubtree(list, index0, Direction.right, stack))
+                    return false;
+                if (list[index0, 2] != -1)
+                    stackOut.Push(list[index0, 2]);
+                if (list[index0, 1] != -1)
+                    stackOut.Push(list[index0, 1]);
+            }
+            return true;
         }
         static void Main(string[] args)
         {
@@ -60,16 +78,29 @@ namespace Task6_3
                     }
                     else
                     {
-                        string[][] list = new string[n][];
+                        int[,] list = new int[n, 3];
                         for (int i = 0; i < n; i++)
                         {
-                            list[i] = sr.ReadLine().Split();
+                            string[] strs = sr.ReadLine().Split();
+
+                            for (int j = 0; j < 3; j++)
+                            {
+                                list[i, j] = int.Parse(strs[j]);
+                                if (j == 1 || j == 2)
+                                {
+                                    list[i, j] -= 1;
+                                }
+                            }
                         }
-                        bool flag = IsSerchTree(list, 0);
-                        if (flag)
+
+                        if (IsSerchTree(list))
+                        {
                             sw.WriteLine("YES");
+                        }
                         else
+                        {
                             sw.WriteLine("NO");
+                        }
                     }
                 }
             }
